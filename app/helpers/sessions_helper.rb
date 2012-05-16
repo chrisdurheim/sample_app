5c4192b88b9672b8da72a9f49e50fc925f723fcc
Chris Durheim <christopherdurheim@gmail.com>
2012-05-16 15:35:22 -0500

    merged form_tag

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 	def sign_in(user)
-		cookies.permanent[:remember_token] = user.remember_token
+		session[:remember_token] = user.remember_token
 		current_user = user
 	end
 	
@@ -13,11 +13,11 @@ module SessionsHelper
 	end
 	
 	def current_user
-		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+		@current_user ||= User.find_by_remember_token(session[:remember_token])
 	end
 	
 	def sign_out
 		current_user = nil
-		cookies.delete(:remember_token)
+		session.delete(:remember_token)
 	end
 end
