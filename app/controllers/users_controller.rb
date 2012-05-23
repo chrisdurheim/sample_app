de6a7f39c5f3eae19a97be7827b4d2b9f8d61521
Chris Durheim <christopherdurheim@gmail.com>
2012-05-23 16:41:01 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
+		@microposts = @user.microposts.paginate(page: params[:page])
 	end
 	  
 	def new
@@ -49,13 +50,6 @@ class UsersController < ApplicationController
   
  private
  
-	def signed_in_user
-		unless signed_in?
-			store_location
-			redirect_to signin_path, notice: "Please sign in." unless signed_in?
-		end
-	end
-	
 	def sign_in_not_wanted
 		redirect_to(root_path) if signed_in?
 	end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,14 @@ module SessionsHelper
 	def signed_in?
 		!current_user.nil?
 	end
-		
+	
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in."
+		end
+	end
+	
 	def current_user=(user)
 		@current_user = user
 	end
