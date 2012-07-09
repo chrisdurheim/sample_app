c66e07c1e74ed3dda45450c92a78387be5c4f4ae
Chris Durheim <christopherdurheim@gmail.com>
2012-07-08 21:21:56 -0500

    ch 10 exercises 1-4

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -4,6 +4,7 @@ describe "MicropostPages" do
 	subject { page }
   
 	let(:user) { FactoryGirl.create(:user) }
+	let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
 	before { sign_in user }
   
 	describe "micropost creation" do
@@ -37,6 +38,15 @@ describe "MicropostPages" do
 			it "should delete a micropost" do
 				expect { click_link "delete" }.should change(Micropost, :count).by(-1)
 			end
+		end
+		
+		describe "as incorrect user" do
+			before do
+				sign_in user
+				visit user_path(wrong_user)
+			end
+			
+			it { should_not have_link('delete') }
 		end
 	end
 end
